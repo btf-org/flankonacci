a6c3f29fbaf088154a1292cc790170cb79ea03c7
Angus Mitchell <anguspmitchell@gmail.com>
2021-08-19 23:35:22 -0500

    install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="nav">
+    <div class="text-red-500">OKAY Tailwind</div>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
