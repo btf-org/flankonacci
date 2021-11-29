6a72ae54b5efa3e8f3d0e0ab42b06f754b1f6a1b
Angus Mitchell <anguspmitchell@gmail.com>
2021-11-29 00:42:15 -0800

    add vuex

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,26 @@ import App from './App.vue'
 import router from './router'
 import "./index.css";
 
-createApp(App).use(router).mount('#app')
+import { createStore } from "vuex";
+
+const defaultState = () => {
+	return {
+		count: 0,
+	};
+};
+
+// Create a new store instance.
+const store = createStore({
+  state: defaultState(),
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
+
+const app = createApp(App);
+app.use(router).mount('#app')
+
+// Install the store instance as a plugin
+app.use(store);
