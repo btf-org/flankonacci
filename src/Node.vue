58be1ee90c4b5cf820abd1c3cf9e065c02a5a584
Angus Mitchell <anguspmitchell@gmail.com>
2021-12-04 00:33:20 -0600

    make the parent/icon text absolute

--- src/Node.vue
+++ src/Node.vue
@@ -6,21 +6,21 @@
       )
     "
   >
-    <div class="absolute top-0 right-0 z-10">
+    <div class="absolute top-0 right-0">
       <button v-if="node.depth > 0" @click="deleteNode">
         <XIcon class="h-5 w-5" />
       </button>
     </div>
-    <div v-if="$store.state.showClasses" class="font-mono">
+    <div
+      v-if="$store.state.showClasses"
+      class="font-mono absolute top-0 left-0"
+    >
       <div>Item: {{ Array.from(node.itemClasses).join(" ") }}</div>
       <div v-if="node?.children.length > 0">
         Parent: {{ Array.from(node.containerClasses).join(" ") }}
       </div>
     </div>
-    <div
-      class="absolute w-full h-full flex flex-row top-0 justify-center z-0"
-      v-if="node?.children.length == 0"
-    >
+    <div class="flex flex-row justify-center" v-if="node?.children.length == 0">
       <button @click="addRow"><DotsHorizontalIcon class="h-5 w-5" /></button>
       <button @click="addColumn"><DotsVerticalIcon class="h-5 w-5" /></button>
     </div>
@@ -29,7 +29,9 @@
       :class="Array.from(node.containerClasses)"
     >
       <Node v-for="child in node.children" :key="child.id" :node="child"></Node>
-      <button @click="addChild"><PlusCircleIcon class="h-5 w-5" /></button>
+      <div>
+        <button @click="addChild"><PlusCircleIcon class="h-5 w-5" /></button>
+      </div>
     </div>
   </div>
 </template>
